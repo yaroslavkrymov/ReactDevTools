b89aa3ebb3bacef03abc786b61e11961cc447133
yaroslavkrymov <93751242+yaroslavkrymov@users.noreply.github.com>
2023-01-09 19:58:24 -0700

    Initial commit

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function Avatar(props) {
+  return <img className="circle-img" src={props.img} alt="avatar_img" />;
+  <Card img={contacts[0].imgURL} />;
+}
+
+export default Avatar;
